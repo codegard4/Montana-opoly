d17dd8954e8a0ec2c2b6279252f2dec4968ff8c0
Cole Odegard <codegard@carroll.edu>
2025-02-16 17:38:52 -0700

    More class scope work

---
+++ src/main/board/Property.java
@@ -0,0 +1,16 @@
+package board;
+from player.Player import Player;
+
+
+public class Property {
+    /*
+     * Constructor for the Property card.
+     * A property card has the property's name, price, rent, owner and number of houses.
+     */
+    private String name;
+    private int price;
+    private int rent;
+    private Player owner;
+    
+
+    }
